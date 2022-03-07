732b90c3ef11e7dce65c82819a88c8ff64c494aa
Lev Evstratov <evstratovlg@gmail.com>
2022-03-08 01:57:35 +0300

    letters multiplier

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,16 +1,27 @@
 package main;
 
-import java.util.stream.IntStream;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isTriangle(1, 2, 2));
-        System.out.println(isTriangle(7, 2, 2));
+        System.out.println(accum("ZpglnRxqenU"));
+        System.out.println(accum("RqaEzty"));
+        System.out.println(accum("cwAt"));
     }
 
-    public static boolean isTriangle(int a, int b, int c) {
-        final int[] sorted = IntStream.of(a, b, c).sorted().toArray();
-        return sorted[0] + sorted[1] > sorted[2];
+    public static String accum(String s) {
+        final char[] letters = s.toCharArray();
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < letters.length; i++) {
+            result.append(String.valueOf(letters[i]).toUpperCase());
+            result.append(String.valueOf(letters[i]).toLowerCase().repeat(i));
+            if (i < letters.length - 1) result.append("-");
+        }
+
+        return result.toString();
     }
+
+//    accum("abcd") -> "A-Bb-Ccc-Dddd"
+//    accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+//    accum("cwAt") -> "C-Ww-Aaa-Tttt"
 }
 
