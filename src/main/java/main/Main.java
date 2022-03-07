692b8791c1167948d05d4517231e5d2fe3bbe40a
Lev Evstratov <evstratovlg@gmail.com>
2022-03-08 01:00:20 +0300

    count vowels

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,19 +2,19 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(check("The quick brown fox jumps over the lazy dog"));
-        System.out.println(check("You shall not pass"));
+        System.out.println(getCount("iaeopuzq"));
+        System.out.println(getCount("etat"));
     }
 
-    public static boolean check(String sentence) {
-        final String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        final String sentenceLetters = sentence.toLowerCase()
-                .chars()
-                .distinct()
-                .sorted()
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
-        return sentenceLetters.contains(alphabet);
+    public static int getCount(String str) {
+        final String vowels = "aeiou";
+
+        int vowelsCount = 0;
+        for (char c : str.toCharArray()) {
+            if (vowels.contains(String.valueOf(c))) vowelsCount++;
+        }
+
+        return vowelsCount;
     }
 }
 
