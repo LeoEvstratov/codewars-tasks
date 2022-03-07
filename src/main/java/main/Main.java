56704f87d120f8eb1a60db4bfac77318066e01bb
Lev Evstratov <evstratovlg@gmail.com>
2022-03-08 00:48:49 +0300

    letters of alphabet

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,20 +1,20 @@
 package main;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Optional;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(findShort("bitcoin take over the world maybe who knows perhaps"));
-        System.out.println(findShort("turns out random test cases are easier than writing out basic ones"));
-        System.out.println(findShort("Let's travel abroad shall we"));
+        System.out.println(check("The quick brown fox jumps over the lazy dog"));
+        System.out.println(check("You shall not pass"));
     }
 
-    public static int findShort(String s) {
-        final String[] words = s.split(" ");
-        final Optional<String> shortestWord = Arrays.stream(words).min(Comparator.comparingInt(String::length));
-        return shortestWord.map(String::length).orElse(0);
+    public static boolean check(String sentence) {
+        final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        final String sentenceLetters = sentence.toLowerCase()
+                .chars()
+                .distinct()
+                .sorted()
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+        return sentenceLetters.contains(alphabet);
     }
 }
 
