58c5de4e6f9a26f04031ba2c813319ef0f7e47dd
Lev Evstratov <evstratovlg@gmail.com>
2022-03-13 19:19:11 +0300

    phone number from int array

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,23 +1,15 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(arrayDiff(new int[]{1, 2}, new int[]{1}));
-        System.out.println(arrayDiff(new int[]{1, 2, 2, 2, 3}, new int[]{2}));
+        System.out.println(createPhoneNumber(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
     }
 
-    public static int[] arrayDiff(int[] a, int[] b) {
-        final List<Integer> bList = Arrays.stream(b).boxed().collect(Collectors.toList());
-
-        final List<Integer> result = Arrays.stream(a).boxed()
-                .filter(i -> !bList.contains(i))
-                .collect(Collectors.toList());
-
-        return result.stream().mapToInt(Integer::intValue).toArray();
+    public static String createPhoneNumber(int[] numbers) {
+        StringBuilder stringNumbers = new StringBuilder();
+        for (int i : numbers) {
+            stringNumbers.append(i);
+        }
+        return String.format("(%s) %s-%s", stringNumbers.substring(0, 3), stringNumbers.substring(3, 6), stringNumbers.substring(6));
     }
 }
