d51a40b06e9225bd9d63f2faf8ce5fe52bcae915
Lev Evstratov <evstratovlg@gmail.com>
2022-03-13 21:30:17 +0300

    usernames like this string

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,21 +2,17 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isPrime(73));
-        System.out.println(isPrime(2));
-        System.out.println(isPrime(75));
-        System.out.println(isPrime(1));
-        System.out.println(isPrime(-1));
-        System.out.println(isPrime(-5));
+//        System.out.println(whoLikesIt("Peter"));
+//        System.out.println(whoLikesIt("Peter", "Alice"));
+//        System.out.println(whoLikesIt("Peter", "Alice, Bob"));
+        System.out.println(whoLikesIt("Peter", "Alice", "Bob", "asdasd", "asd"));
     }
 
-    public static boolean isPrime(int num) {
-        if (num < 2) return false;
-
-        final int sqrt = (int) Math.sqrt(num);
-        for (int i = sqrt; i > 1; i--) {
-            if (num % i == 0) return false;
-        }
-        return true;
+    public static String whoLikesIt(String... names) {
+        if (names.length == 0) return "no one likes this";
+        if (names.length == 1) return String.format("%s likes this", names[0]);
+        if (names.length == 2) return String.format("%s and %s like this", names[0], names[1]);
+        if (names.length == 3) return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+        return String.format("%s, %s and %d others like this", names[0], names[1], names.length - 2);
     }
 }
